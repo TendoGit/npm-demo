aae9c25c9d28fe174061b87805e424545e27030b
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-16 09:51:36 +0200

    added a course route displaying a simple array

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
 app.listen(5000, () => console.log('Listening on port 5000...'));
