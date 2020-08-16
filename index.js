573ec305037e7ccc35383778afdbee99a83504c2
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-16 10:21:05 +0200

    added a single course route with route parameters

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
 
-app.listen(5000, () => console.log('Listening on port 5000...'));
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Listening on port ${port}...`));
