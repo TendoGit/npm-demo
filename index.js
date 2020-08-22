3115080e0bbb16708dab4e690333e1934e4ca51e
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-22 13:47:22 +0200

    Recatoring: Home Route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
 const courses = require('./routes/courses');
+const home = require('./routes/home');
 const express =  require('express');
 const app = express();
 
@@ -13,23 +14,16 @@ app.set('views', './views');
 app.use(express.json());
 app.use(helmet());
 app.use('/api/courses', courses);
+app.use('/', home);
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
     debug('Morgan enabled...')
 }
 
-
 //Configuration
 console.log(`Application Name: ${config.get('name')}`);
 console.log(`Mail Server: ${config.get('mail.host')}`);
 console.log(`Mail Password: ${config.get('mail.password')}`);
-
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'My Express App',
-        message: 'Welcome'
-    });
-});
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
